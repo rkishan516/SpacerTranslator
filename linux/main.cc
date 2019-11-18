47acf952fc0e6873a47c8f38fc7a42e15b2b6b7b
kishan <rkishan516@gmail.com>
2019-11-18 20:34:34 +0530

    Added better Support for Desktop App

--- linux/main.cc
+++ linux/main.cc
@@ -11,31 +11,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <flutter/flutter_window_controller.h>
+#include <linux/limits.h>
+#include <unistd.h>
 
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <memory>
 #include <vector>
 
-#include "flutter/flutter_window_controller.h"
-
-// Include windows.h last, to minimize potential conflicts. The CreateWindow
-// macro needs to be undefined because it prevents calling
-// FlutterWindowController's method.
-#include <windows.h>
-#undef CreateWindow
+#include "flutter/generated_plugin_registrant.h"
 
 namespace {
 
 // Returns the path of the directory containing this executable, or an empty
 // string if the directory cannot be found.
 std::string GetExecutableDirectory() {
-  char buffer[MAX_PATH];
-  if (GetModuleFileName(nullptr, buffer, MAX_PATH) == 0) {
+  char buffer[PATH_MAX + 1];
+  ssize_t length = readlink("/proc/self/exe", buffer, sizeof(buffer));
+  if (length > PATH_MAX) {
     std::cerr << "Couldn't locate executable" << std::endl;
     return "";
   }
-  std::string executable_path(buffer);
-  size_t last_separator_position = executable_path.find_last_of('\\');
+  std::string executable_path(buffer, length);
+  size_t last_separator_position = executable_path.find_last_of('/');
   if (last_separator_position == std::string::npos) {
     std::cerr << "Unabled to find parent directory of " << executable_path
               << std::endl;
@@ -52,22 +51,29 @@ int main(int argc, char **argv) {
   if (base_directory.empty()) {
     base_directory = ".";
   }
-  std::string data_directory = base_directory + "\\data";
-  std::string assets_path = data_directory + "\\flutter_assets";
-  std::string icu_data_path = data_directory + "\\icudtl.dat";
+  std::string data_directory = base_directory + "/data";
+  std::string assets_path = data_directory + "/flutter_assets";
+  std::string icu_data_path = data_directory + "/icudtl.dat";
 
   // Arguments for the Flutter Engine.
   std::vector<std::string> arguments;
 
   flutter::FlutterWindowController flutter_controller(icu_data_path);
+  flutter::WindowProperties window_properties = {};
+  window_properties.title = "Flutter Desktop Example";
+  window_properties.width = 800;
+  window_properties.height = 600;
 
   // Start the engine.
-  if (!flutter_controller.CreateWindow(800, 600, "Flutter Desktop Example",
-                                       assets_path, arguments)) {
+  if (!flutter_controller.CreateWindow(window_properties, assets_path,
+                                       arguments)) {
     return EXIT_FAILURE;
   }
+  RegisterPlugins(&flutter_controller);
 
   // Run until the window is closed.
-  flutter_controller.RunEventLoop();
+  while (flutter_controller.RunEventLoopWithTimeout(
+      std::chrono::milliseconds::max())) {
+  }
   return EXIT_SUCCESS;
 }
